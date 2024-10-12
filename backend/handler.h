55b7baea74949c85a5504556e1b654e6e17e35fb
windinjuries <294394591@qq.com>
2024-10-12 08:21:42 +0800

    feat: backend response use json

--- backend/handler.h
+++ backend/handler.h
@@ -1,12 +1,27 @@
 #ifndef HANDLER_H
 #define HANDLER_H
 
+#include "oatpp/parser/json/mapping/ObjectMapper.hpp"
 #include "oatpp/web/server/HttpRequestHandler.hpp"
-#include <nlohmann/json.hpp>
+
+#include "oatpp/core/Types.hpp"
+#include "oatpp/core/macro/codegen.hpp"
+#include <stdio.h>
+
 
 #define O_UNUSED(x) (void)x;
 
-using json = nlohmann::json;
+#include OATPP_CODEGEN_BEGIN(DTO) ///< Begin DTO codegen section
+class UserDto : public oatpp::DTO {
+
+  DTO_INIT(UserDto, DTO)
+
+  DTO_FIELD(String, name,"user_name");
+  DTO_FIELD(UInt32, id, "id");
+
+};
+#include OATPP_CODEGEN_END(DTO) ///< END DTO codegen section
+
 // 自定义请求处理程序
 class Handler : public oatpp::web::server::HttpRequestHandler
 {
@@ -15,8 +30,15 @@ public:
     std::shared_ptr<OutgoingResponse> handle(const std::shared_ptr<IncomingRequest> &request) override
     {
         O_UNUSED(request);
+        auto user = UserDto::createShared();
+        user->name = "Ivan";
+        user->id = 1;
+        auto jsonObjectMapper = oatpp::parser::json::mapping::ObjectMapper::createShared();
+        oatpp::String json = jsonObjectMapper->writeToString(user); 
+        printf("%s\n", json->c_str()); ///< print json
 
-        return ResponseFactory::createResponse(Status::CODE_200, "Hello, World!");
+        return ResponseFactory::createResponse(Status::CODE_200, json);
+        
     }
 };
 
